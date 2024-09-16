2f456e4e4e1bd54a0016126a9aae96b0614cb898
mrico98 <manuelrico98@outlook.es>
2024-09-15 21:12:54 -0600

    adding plugins folder

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+      })
+    end,
+  },
+}
